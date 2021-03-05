cba9b927c29b1f5fe873d88536fc86e7c2238e5f
eduardo-ewerton-oodle <eduardo.ewerton@oodlefinance.com>
2021-03-05 16:23:43 +0000

    added logo and few layout fixes

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {FileUploader} from "ng2-file-upload";
-import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FileUploader} from 'ng2-file-upload';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-file-upload',
@@ -13,34 +13,36 @@ export class FileUploadComponent implements OnInit {
 
   uploadForm: FormGroup;
 
-  public uploader:FileUploader = new FileUploader({
+  public uploader: FileUploader = new FileUploader({
     isHTML5: true
   });
-  title: string = 'File Upload';
-  constructor(private fb: FormBuilder, private http: HttpClient ) { }
+  title = 'File Upload';
 
-  uploadSubmit(){
-        for (var i = 0; i < this.uploader.queue.length; i++) {
-          let fileItem = this.uploader.queue[i]._file;
-          if(fileItem.size > 10000000){
-            alert("Each File should be less than 10 MB of size.");
-            return;
-          }
-        }
-        for (var j = 0; j < this.uploader.queue.length; j++) {
-          let data = new FormData();
-          let fileItem = this.uploader.queue[j]._file;
-          console.log(fileItem.name);
-          data.append('file', fileItem);
-          data.append('fileSeq', 'seq'+j);
-          data.append( 'dataType', this.uploadForm.controls.type.value);
-          this.uploadFile(data).subscribe(data => alert(data.message));
-        }
-        this.uploader.clearQueue();
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+  }
+
+  uploadSubmit() {
+    for (let i = 0; i < this.uploader.queue.length; i++) {
+      const fileItem = this.uploader.queue[i]._file;
+      if (fileItem.size > 10000000) {
+        alert('Each File should be less than 10 MB of size.');
+        return;
+      }
+    }
+    for (let j = 0; j < this.uploader.queue.length; j++) {
+      const data = new FormData();
+      const fileItem = this.uploader.queue[j]._file;
+      console.log(fileItem.name);
+      data.append('file', fileItem);
+      data.append('fileSeq', 'seq' + j);
+      data.append('dataType', this.uploadForm.controls.type.value);
+      this.uploadFile(data).subscribe(data => alert(data.message));
+    }
+    this.uploader.clearQueue();
   }
 
   uploadFile(data: FormData): Observable<any> {
-    //debugger
+    // debugger
     return this.http.post<any>('https://sac.rebisconsulting.com/tools/upload/upload.php', data);
   }
 
@@ -48,8 +50,8 @@ export class FileUploadComponent implements OnInit {
     this.uploadForm = this.fb.group({
       document: [null, null],
       type: 'File',
-      source:  [null, Validators.compose([Validators.required])],
-      destination:  [null, Validators.compose([Validators.required])]
+      source: [null, Validators.compose([Validators.required])],
+      destination: [null, Validators.compose([Validators.required])]
     });
   }
 
